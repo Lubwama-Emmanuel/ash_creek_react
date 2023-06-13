e1dc390162d07dcad516b9ea2dde1c9f6498de5b
Rex <lubwamaemmanuel1@gmail.com>
2023-06-13 15:02:47 +0300

    refactor:(a little clean up)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import Across from "./Components/Across";
+import BusinessModel from "./Components/BusinessModel";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
+import Investments from "./Components/Investments";
+import Merchant from "./Components/Merchant";
+import Portfolio from "./Components/Portfolio";
+import WhoAreWe from "./Components/WhoAreWe";
+import Members from "./Components/Members";
+
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Hero />
+      <Merchant />
+      <BusinessModel />
+      <Investments />
+      <Across />
+      <Portfolio />
+      <WhoAreWe />
+      <Members />
+      <CookieConsent debug={true}>This site uses cookies.</CookieConsent>
     </div>
   );
 }
